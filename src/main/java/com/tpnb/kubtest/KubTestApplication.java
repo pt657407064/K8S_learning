cac93556b2503bf569d65c025a72cbac2944da5c
Tao Pan <tpan@salesforce.com>
2019-12-22 01:37:01 -0800

    Init

---
+++ src/main/java/com/tpnb/kubtest/KubTestApplication.java
@@ -0,0 +1,13 @@
+package com.tpnb.kubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpnb/kubtest/KubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpnb.kubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
